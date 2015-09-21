de09f8179e6e82200e194a51f3c71ae08e14db5a
Rahul Horé <hore.rahul@gmail.com>
2015-09-20 21:55:16 -0400

    Bootstrap Twitter widget

--- jobs/twitter.rb
+++ jobs/twitter.rb
@@ -1,26 +1,34 @@
 require 'twitter'
-
+require 'dotenv'
+Dotenv.load
 
 #### Get your twitter keys & secrets:
 #### https://dev.twitter.com/docs/auth/tokens-devtwittercom
 twitter = Twitter::REST::Client.new do |config|
-  config.consumer_key = 'YOUR_CONSUMER_KEY'
-  config.consumer_secret = 'YOUR_CONSUMER_SECRET'
-  config.access_token = 'YOUR_OAUTH_TOKEN'
-  config.access_token_secret = 'YOUR_OAUTH_SECRET'
+  config.consumer_key        = ENV['CONSUMER_KEY']
+  config.consumer_secret     = ENV['CONSUMER_SECRET']
+  config.access_token        = ENV['ACCESS_TOKEN']
+  config.access_token_secret = ENV['ACCESS_SECRET']
 end
 
-search_term = URI::encode('#todayilearned')
+search_term = URI::encode('random')
+tweet_count = 50
 
 SCHEDULER.every '10m', :first_in => 0 do |job|
   begin
-    tweets = twitter.search("#{search_term}")
+    tweets = twitter.search("#{search_term}", result_type: 'recent').take(tweet_count)
 
     if tweets
-      tweets = tweets.map do |tweet|
-        { name: tweet.user.name, body: tweet.text, avatar: tweet.user.profile_image_url_https }
+      tweets.map! do |tweet|
+        { name:           tweet.user.name,
+          body:           tweet.text,
+          avatar:         tweet.user.profile_image_url_https,
+          created_at:     tweet.created_at,
+          favorite_count: tweet.favorite_count,
+          retweet_count:  tweet.retweet_count,
+          media:          (tweet.attrs[:entities][:media].sample[:display_url] if tweet.attrs[:entities][:media]) }
       end
-      send_event('twitter_mentions', comments: tweets)
+      send_event('synchro_twitter', tweets: tweets)
     end
   rescue Twitter::Error
     puts "\e[33mFor the twitter widget to work, you need to put in your twitter API keys in the jobs/twitter.rb file.\e[0m"
